2ebd55b82e08c6dae7bef96c685949c15dfd6df5
Luke Thompson <lthompson@infomedia.com.au>
2020-11-19 05:37:42 +0000

    add real files (not link)

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// Add your API invoke URL below, configured in Module 3. 
+
+module.exports = {
+    api: {
+        invokeUrl: 'https://gmpo4m6z55.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+    }
+}
